38c5c3203ade154ead714d6e3855d44425a84bc4
Rita HC <ritahameerchauhan@gmail.com>
2023-01-19 19:50:54 -0500

    Buttons working

---
+++ js/app.js
@@ -0,0 +1,123 @@
+console.log('Linked')
+
+const fanpageInfo = document.getElementById('fanpageInfo');
+const background = document.getElementById('background');
+
+const toggleScreen = (id, toggle) => {
+    let element = document.getElementById(id)
+    let display1 = (toggle) ? 'block' : 'none'
+    element.style.display = display1
+}
+
+
+
+const row1 = document.getElementById('row1')
+const row2 = document.getElementById('row2')
+
+const rheanyra = document.getElementById('rheanyra')
+const daemon = document.getElementById('daemon')
+const rheanys = document.getElementById('rheanys')
+const alicent = document.getElementById('alicent')
+const otto = document.getElementById('otto')
+const criston = document.getElementById('criston')
+
+///////////// Rhaenyra ///////////////////////////
+const renBio = document.getElementById('renBio')
+const renSta = document.getElementById('renSta')
+const renBioData = document.getElementById('renBioData')
+
+const RhaenyraBio = () => {
+    console.log('Click1')
+    toggleScreen('daemon', false)
+    toggleScreen('renSta', false)
+    toggleScreen('rheanys', false)
+    toggleScreen('row2', false)
+    toggleScreen('renBioData', true)
+    console.log(renBioData)
+}
+
+
+///////////// Daemon ///////////////////////////
+
+const deaBio = document.getElementById('deaBio')
+const deaBioData = document.getElementById('deaBioData')
+const deaSta = document.getElementById('deaSta')
+
+const deamonBio = () => {
+    console.log('Click2')
+    toggleScreen('daemon', true)
+    toggleScreen('deaSta', false)
+    toggleScreen('rheanyra', false)
+    toggleScreen('rheanys', false)
+    toggleScreen('row2', false)
+    toggleScreen('deaBioData', true)
+    console.log(deaBioData)
+}
+
+///////////// Rhaenys ///////////////////////////
+
+const rhaBio = document.getElementById('rhaBio')
+const rhaBioData = document.getElementById('rhaBioData')
+const rhaSta = document.getElementById('rhaSta')
+
+const rhaenysBio = () => {
+    console.log('Click2')
+    toggleScreen('daemon', false)
+    toggleScreen('rhaSta', false)
+    toggleScreen('rheanyra', false)
+    toggleScreen('rheanys', true)
+    toggleScreen('row2', false)
+    toggleScreen('rhaBioData', true)
+    console.log(deaBioData)
+}
+
+///////////// Alicent ///////////////////////////
+
+const aliBio = document.getElementById('aliBio')
+const aliBioData = document.getElementById('aliBioData')
+const aliSta = document.getElementById('aliSta')
+
+const alicentBio = () => {
+    console.log('Click2')
+    toggleScreen('criston', false)
+    toggleScreen('aliSta', false)
+    toggleScreen('otto', false)
+    toggleScreen('alicent', true)
+    toggleScreen('row1', false)
+    toggleScreen('aliBioData', true)
+    console.log(deaBioData)
+}
+
+///////////// Otto ///////////////////////////
+
+const ottBio = document.getElementById('ottBio')
+const ottBioData = document.getElementById('ottBioData')
+const ottSta = document.getElementById('ottSta')
+
+const ottoBio = () => {
+    console.log('Click2')
+    toggleScreen('alicent', false)
+    toggleScreen('ottSta', false)
+    toggleScreen('criston', false)
+    toggleScreen('otto', true)
+    toggleScreen('row1', false)
+    toggleScreen('ottBioData', true)
+    console.log(deaBioData)
+}
+
+///////////// Criston ///////////////////////////
+
+const criBio = document.getElementById('criBio')
+const criBioData = document.getElementById('criBioData')
+const criSta = document.getElementById('criSta')
+
+const cristonBio = () => {
+    console.log('Click2')
+    toggleScreen('alicent', false)
+    toggleScreen('criSta', false)
+    toggleScreen('otto', false)
+    toggleScreen('criston', true)
+    toggleScreen('row1', false)
+    toggleScreen('criBioData', true)
+    console.log(deaBioData)
+}
